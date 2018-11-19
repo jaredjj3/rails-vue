12052d3bdb078db12b320b24565348332dcbe0aa
Jared Johnson <jared@nuts.com>
2018-11-19 17:28:14 -0500

    Flesh out order line statuses controller create action.

--- app/controllers/order_line_statuses_controller.rb
+++ app/controllers/order_line_statuses_controller.rb
@@ -1,4 +1,15 @@
 class OrderLineStatusesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
+    respond_to do |format|
+      format.json do
+        if rand > 0.5
+          render(json: { foo: :bar })
+        else
+          render(json: { errors: :baz }, status: :bad_request)
+        end
+      end
+    end
   end
 end
