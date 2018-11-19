819f336813e6f1476dd8e39bd1a50c55023eff07
Jared Johnson <jared@nuts.com>
2018-11-19 11:09:32 -0500

    Root to StaticPagesController#root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/root'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
 end
